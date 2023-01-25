a66904da80c2391a3271b4d12b8f0933eda0191f
Matyrobbrt <matyrobbrt@gmail.com>
2023-01-25 21:05:38 +0200

    Remove `Meta` annotations

--- src/main/java/com/matyrobbrt/website/AppShell.java
+++ src/main/java/com/matyrobbrt/website/AppShell.java
@@ -1,7 +1,6 @@
 package com.matyrobbrt.website;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.component.page.Meta;
 import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
@@ -9,11 +8,6 @@ import com.vaadin.flow.theme.lumo.Lumo;
 
 @Theme(variant = Lumo.DARK)
 @PWA(name = "Matyrobbrt's website", shortName = "Matyrobbrt's website")
-@Meta(name = "og:title", content = "Forge MDK Maker")
-@Meta(name = "og:type", content = "website")
-@Meta(name = "og:url", content = "https://matyrobbrt.com/projects/forge-mdk")
-@Meta(name = "og:description", content = "A creator for Forge MDKs")
-@Meta(name = "theme-color", content = "#FF0000")
 public class AppShell implements AppShellConfigurator {
     @Override
     public void configurePage(AppShellSettings settings) {
